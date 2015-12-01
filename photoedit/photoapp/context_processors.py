0008872e3f21d4923233813fef723747302033d0
Samuel James <samuel.james@andela.com>
2015-12-01 21:55:00 +0100

    make effects selectable

--- photoedit/photoapp/context_processors.py
+++ photoedit/photoapp/context_processors.py
@@ -25,6 +25,18 @@ Image_Effects = dict(
             "class": "thumbnail inline", "format": "jpg", "crop": "fill", "height": 130, "width": 130,
         },
 
+
+        default=dict(
+          format='png',
+          ),
+
+        NORMAL=dict(
+          format = "png",
+          transformation = [
+              dict(height=130, width=130, radius=10,),
+          ]
+          ),
+        #EFFECTS, EFFECT1
         SMALL_SERPIA = dict(
           format = "png",
           transformation = [
@@ -38,19 +50,140 @@ Image_Effects = dict(
               dict(crop="thumb",effect = "sepia"),
           ]
         ),
-
-        default=dict(
-          format='png',
-          ),
-
-        NORMAL=dict(
+        #EFFECT2
+        S_GRAYSCALE = dict(
           format = "png",
           transformation = [
-              dict(height=130, width=130, radius=10,),
+              dict(height=130, width=130, crop="thumb", radius=10, effect = "grayscale"),
           ]
-          ),
+        ),
 
-    )
+        B_GRAYSCALE = dict(
+          format = "png",
+          transformation = [
+              dict(crop="thumb",effect = "grayscale"),
+          ]
+        ),
+      #EFFECT3
+      S_VIGNETTE = dict(
+          format = "png",
+          transformation = [
+              dict(height=130, width=130, crop="thumb", radius=10, effect = "vignette"),
+          ]
+        ),
+
+        B_VIGNETTE = dict(
+          format = "png",
+          transformation = [
+              dict(crop="thumb",effect = "vignette"),
+          ]
+        ),
+
+      #EFFECT4
+      S_GRADIENTFADE = dict(
+          format = "png",
+          transformation = [
+              dict(height=130, width=130, crop="thumb", radius=10, effect = "gradient_fade"),
+          ]
+        ),
+
+        B_GRADIENTFADE = dict(
+          format = "png",
+          transformation = [
+              dict(crop="thumb",effect = "gradient_fade"),
+          ]
+        ),
+
+      #EFFECT5
+      S_CONTRAST_70 = dict(
+          format = "png",
+          transformation = [
+              dict(height=130, width=130, crop="thumb", radius=10, effect = "contrast:-70"),
+          ]
+        ),
+
+        B_CONTRAST_70 = dict(
+          format = "png",
+          transformation = [
+              dict(crop="thumb",effect = "contrast:-70"),
+          ]
+        ),
 
 
+        #FILTER1
+        S_BRIGHTNESS60 = dict(
+          format = "png",
+          transformation = [
+              dict(height=130, width=130, crop="thumb", radius=10, effect = "brightness:60"),
+          ]
+        ),
+
+        B_BRIGHTNESS60 = dict(
+          format = "png",
+          transformation = [
+              dict(crop="thumb",effect = "brightness:60"),
+          ]
+        ),
+    
+        #FILTER2
+        S_BLUR = dict(
+          format = "png",
+          transformation = [
+              dict(height=130, width=130, crop="thumb", radius=10, effect = "blur:200"),
+          ]
+        ),
+
+        B_BLUR = dict(
+          format = "png",
+          transformation = [
+              dict(crop="thumb",effect = "blur:200"),
+          ]
+        ),
+
+        #FILTER3
+        S_RED = dict(
+          format = "png",
+          transformation = [
+              dict(height=130, width=130, crop="thumb", radius=10, effect = "red:50"),
+          ]
+        ),
+
+        B_RED = dict(
+          format = "png",
+          transformation = [
+              dict(crop="thumb",effect = "red:50"),
+          ]
+        ),
+
+        #FILTER4
+        S_GREEN = dict(
+          format = "png",
+          transformation = [
+              dict(height=130, width=130, crop="thumb", radius=10, effect = "green:50"),
+          ]
+        ),
+
+        B_GREEN = dict(
+          format = "png",
+          transformation = [
+              dict(crop="thumb",effect = "green:90"),
+          ]
+        ),
+
+        #FILTER5
+        S_BLUE = dict(
+          format = "png",
+          transformation = [
+              dict(height=130, width=130, crop="thumb", radius=10, effect = "blue:90"),
+          ]
+        ),
+
+        B_BLUE = dict(
+          format = "png",
+          transformation = [
+              dict(crop="thumb",effect = "blue:90"),
+          ]
+        ),
+
+)
 
